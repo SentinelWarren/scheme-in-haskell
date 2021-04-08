1a18afe37844ace011971eec10b95ccae50a2e98
SentinelWarren <warrenkalolo@gmail.com>
2021-04-08 04:03:00 +0300

    Refactor

--- src/main.hs
+++ src/main.hs
@@ -19,49 +19,34 @@ data LispVal = Atom String
 
 parseString :: Parser LispVal
 parseString = do
-                char '"'
-                x <- many (noneOf "\"")
-                char '"'
-                return $ String x
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
 
 parseAtom :: Parser LispVal
 parseAtom = do
-              first <- letter <|> symbol
-              rest <- many (letter <|> digit <|> symbol)
-              let atom = [first] ++ rest
-              return $ case atom of
-                "#t" -> Bool True
-                "#f" -> Bool False
-                otherwise -> Atom atom
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = [first] ++ rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _    -> Atom atom
 
-parseNumber :: Parser LispVal   
+parseNumber :: Parser LispVal
 parseNumber = do
-                x <- many1 digit
-                return $ Number $ read x
+  x <- many1 digit
+  return $ Number $ read x
 --parseNumber = liftM (Number . read) $ many1 digit
 
 parseFloat :: Parser LispVal
 parseFloat = do
-                x <- many1 digit
-                char '.'
-                y <- many1 digit
-                let atom = (x ++ "." ++ y)
-                return $ Float $ read atom
-
-parseList :: Parser LispVal
-parseList = liftM List $ sepBy parseExpr spaces
-
-parseDottedList :: Parser LispVal
-parseDottedList = do
-                    head <- endBy parseExpr spaces
-                    tail <- char '.' >> spaces >> parseExpr
-                    return $ DottedList head tail
-
-parseQuoted :: Parser LispVal
-parseQuoted = do
-                char '\''
-                x <- parseExpr
-                return $ List [Atom "quote", x]
+  x <- many1 digit
+  char '.'
+  y <- many1 digit
+  let atom = (x ++ "." ++ y)
+  return $ Float $ read atom
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
@@ -70,9 +55,24 @@ parseExpr = parseAtom
         <|> parseNumber
         <|> parseQuoted
         <|> do char '('
-               x <- (try parseList) <|> parseDottedList
+               x <- try parseList <|> parseDottedList
                char ')'
                return x
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+  head <- endBy parseExpr spaces
+  tail <- char '.' >> spaces >> parseExpr
+  return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  char '\''
+  x <- parseExpr
+  return $ List [Atom "quote", x]
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -82,13 +82,13 @@ symbol = oneOf "!$%&|*+/:<=?>@^_~#"
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found value: " ++ show val
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value: " ++ show val
 
 
 -- ****** MAIN | REPL ******
 
 main :: IO ()
 main = do
-        args <- getArgs
-        putStrLn (readExpr (args !! 0))
+  args <- getArgs
+  putStrLn (readExpr (args !! 0))
