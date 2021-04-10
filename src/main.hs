9fdd0c27f3d215c3aa56e996284f6954826da229
SentinelWarren <warrenkalolo@gmail.com>
2021-04-10 07:32:35 +0300

    Add equal implementation

--- src/main.hs
+++ src/main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances, ExistentialQuantification #-}
 module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
@@ -13,8 +14,6 @@ data LispVal = Atom String
              | Float Float
              | String String
              | Bool Bool
-            --  deriving Show
-
 data LispError = NumArgs Integer [LispVal]
                | TypeMismatch String LispVal
                | Parser ParseError
@@ -22,7 +21,8 @@ data LispError = NumArgs Integer [LispVal]
                | NotFunction String String
                | UnboundVar String String
                | Default String
-              --  deriving Show
+
+data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
 
 
 -- ****** Parser ******
@@ -154,6 +154,13 @@ unpackBool :: LispVal -> ThrowsError Bool
 unpackBool (Bool b) = return b
 unpackBool notBool  = throwError $ TypeMismatch "boolean" notBool
 
+unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool
+unpackEquals arg1 arg2 (AnyUnpacker unpacker) = do
+  unpacked1 <- unpacker arg1
+  unpacked2 <- unpacker arg2
+  return $ unpacked1 == unpacked2
+  `catchError` (const $ return False)
+
 car :: [LispVal] -> ThrowsError LispVal
 car [List(x : xs)] = return x
 car [DottedList (x : xs) _] = return x
@@ -187,6 +194,14 @@ eqv [(List arg1), (List arg2)] = return $ Bool $ (length arg1 == length arg2) &&
                             Right (Bool val) -> val                         
 eqv [_, _] = return $ Bool False
 eqv badArgList = throwError $ NumArgs 2 badArgList
+
+equal :: [LispVal] -> ThrowsError LispVal
+equal [arg1, arg2] = do
+  primitiveEquals <- liftM or $ mapM (unpackEquals arg1 arg2) 
+                      [AnyUnpacker unpackNum, AnyUnpacker unpackStr, AnyUnpacker unpackBool]
+  eqvEquals <- eqv [arg1, arg2]
+  return $ Bool $ (primitiveEquals || let (Bool x) = eqvEquals in x)
+equal badArgList = throwError $ NumArgs 2 badArgList
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numericBinop op            [] = throwError $ NumArgs 2 []
@@ -230,7 +245,8 @@ primitives = [("+", numericBinop (+)),
               ("cdr", cdr),
               ("cons", cons),
               ("eq?", eqv),
-              ("eqv?", eqv)]
+              ("eqv?", eqv),
+              ("equal?", equal)]
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function args" func)
