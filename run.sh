740ba7d2c8df0b2b8f6a1da5a5ae4b4756053be3
SentinelWarren <warrenkalolo@gmail.com>
2021-04-13 04:30:22 +0300

    Updates run script to run tests

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+if [ $1 == "--test" ];
+then
+    ghc src/Main.hs
+    runhaskell -i./src test/Spec.hs
+else
+    ghc src/Main.hs
+    rlwrap src/Main
+fi
+
+# rlwrap ./main "$1"
--- src/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-ghc main.hs
-rlwrap ./main
-
-# rlwrap ./main "$1"
